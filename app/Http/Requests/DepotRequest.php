b7c1a022d2b4ac2acc9b1ca0c33a19fc31661055
Gaëlle Pierlot <gaellepierlot@gmail.com>
2020-01-13 11:28:19 +0400

    [Dépôt de projet] Amélioration de la validation des champs de saisie

--- app/Http/Requests/DepotRequest.php
+++ app/Http/Requests/DepotRequest.php
@@ -27,11 +27,36 @@ class DepotRequest extends FormRequest
             'titre_projet' => 'required|alpha_dash',
             'type_projet' => 'required',
             'desc_projet' => 'required|alpha_dash',
-            'date_debut' => 'date|after:yesterday',
+            'date_debut' => 'date|after_or_equal:today',
             'date_butoir_projet' => 'required|date|after:date_debut',
             'Budget_min_projet' => 'numeric',
             'Budget_max_projet' => 'required|numeric',
             'g-recaptcha-response' => 'required',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'titre_projet' => 'titre du projet',
+            'type_projet' => 'type du projet',
+            'desc_projet' => 'description',
+            'date_debut' => 'date de début',
+            'date_butoir_projet' => 'date butoir',
+            'Budget_min_projet' => 'budget minimum',
+            'Budget_max_projet' => 'budget maximum',
+        ]; 
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'date_debut.after_or_equal' => 'Le champ :attribute doit être une date postérieure ou égale à aujourd\'hui.',
         ];
     }
 }
