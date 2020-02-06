8eb6cd10ab080289ce61cd24fb61313c8446f1d1
Gaëlle Pierlot <gaellepierlot@gmail.com>
2020-02-06 11:19:33 +0400

    Suppression de la table formations

--- database/migrations/2020_02_06_060522_create_realisateur_projets_table.php
+++ database/migrations/2020_02_06_060522_create_realisateur_projets_table.php
@@ -22,13 +22,13 @@ class CreateRealisateurProjetsTable extends Migration
             $table->string('Mdp');
             $table->double('Telephone')->nullable();
             $table->date('DateNaissance')->nullable();
+            $table->string('formations')->nullable();
             $table->string('CVURL')->nullable();
             $table->string('LinkedinURL')->nullable();
             $table->integer('NbProjets');
             $table->integer('NbPoints');
             $table->integer('IdNiveauEtudes')->unsigned();
             $table->integer('IdDiplomes')->unsigned();
-            $table->integer('IdFormations')->unsigned();
             $table->integer('IdOrga')->unsigned();
             $table->integer('IdStatut')->unsigned();
             $table->integer('IdDomaine')->unsigned();
@@ -36,7 +36,6 @@ class CreateRealisateurProjetsTable extends Migration
             $table->foreign('IdStatut')->references('id')->on('statut_realisateurs');
             $table->foreign('IdNiveauEtudes')->references('Id')->on('statut_realisateurs');
             $table->foreign('IdDiplomes')->references('Id')->on('diplomes');
-            $table->foreign('IdFormations')->references('Id')->on('formations');
             $table->foreign('IdDomaine')->references('Id')->on('domaines');
             $table->timestamps();
         });
