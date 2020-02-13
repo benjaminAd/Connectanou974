a6086e3ad918cc6ff3011728c10880da2cf461fb
Gaëlle Pierlot <gaellepierlot@gmail.com>
2020-02-13 20:07:50 +0400

    Amélioration du seeder StatuRealisateur

---
+++ database/seeds/StatutRealisateursTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\StatutRealisateur;
+
+class StatutRealisateursTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $statuts_realisateur = [
+            'Etudiant',
+            'Formateur',
+            'Salarié',
+            'Demandeur d\'emploi',
+            'Entrepreneur'
+        ];
+        $id = 1;
+
+        foreach ($statuts_realisateur as $statut) {
+            StatutRealisateur::create([
+                'id' => $id++,
+                'Statut' => $statut
+            ]);
+        }
+    }
+    }
+}
